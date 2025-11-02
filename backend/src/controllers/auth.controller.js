21545590ba8c37480e6e8c3189d47dd971223f8a
Divyanshu <dnd.mein@gmail.com>
2025-11-02 15:03:16 +0530

    signup section code refactor

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -26,8 +26,13 @@ export const signup=async (req, res) => {
                 password:hashedPassword,
             })
             if(newUser){
-                generateToken(newUser._id,res)
-                await newUser.save();
+
+                // generateToken(newUser._id,res)
+                // await newUser.save();
+
+                //persist user first
+                const savedUser=await newUser.save();
+                generateToken(savedUser._id,res);
                 res.status(201).json({
                     _id:newUser._id,
                     fullName:newUser.fullName,
